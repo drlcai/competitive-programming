a23854a9d9af0c419a04aeda459c1c8900828238
Leran Cai <chuckcai@yahoo.com>
2021-01-07 12:56:04 +0000

    Update quickstart template and problem 1B

--- Codeforces/cpp/1B/1B.cpp
+++ Codeforces/cpp/1B/1B.cpp
@@ -51,6 +51,7 @@ using namespace std;
 #define read(type) readInt<type>()
 const double pi=acos(-1.0);
 typedef pair<int, int> PII;
+typedef pair<string, string> PSS;
 typedef vector<int> VI;
 typedef vector<string> VS;
 typedef vector<PII> VII;
@@ -64,26 +65,91 @@ typedef long long int int64;
 typedef unsigned long long int uint64;
 
 #define watch(x) cout << (#x) << " is " << (x) << endl;
-
 /********** Main()  function **********/
-void solve() {
 
+string parseR(int t, string r){
+  string res;
+  if(t == 0){
+    res = "R" + r;
+  }else{
+    res = r;
+  }
+  return res;
+}
+
+string parseC(int t, string c){
+  string res = "";
+  if (t == 0){ // AB
+    int sum = 0;
+    FOR(i, 0, c.size(), 1){
+      sum = sum * 26 + (c[i] - 'A' + 1);
+    }
+    res = "C" + to_string(sum);
+  }
+  else{ //23
+    int ic = stoi(c);
+    while(ic != 0){
+      int r = (ic - 1) % 26;
+      res = char(('A' + r)) + res;
+      ic = (ic - 1) / 26;
+    }
+  }
+  return res;
+}
+
+string parse(string s){
+  string res;
+  if (s[0] != 'R' || isalpha(s[1])){ // BC23, RA23 (type 0)
+    string col = "";
+    string row = "";
+    int ptr = 0;
+    while(isalpha(s[ptr])){
+      col += s[ptr++];
+    }
+    while(ptr < s.size()){
+      row += s[ptr++];
+    }
+    res = parseR(0, row) + parseC(0, col);
+  }
+  else{ // R23 (type 0) or R12C23 (type 1)
+    int ptr = 1;
+    string row = "";
+    while (isdigit(s[ptr]) && ptr < s.size()){
+      row += s[ptr];
+      ptr ++;
+    }
+    if (ptr == s.size()){ // type 0
+      res = parseR(0, row) + parseC(0, "R");
+    }else{ // type 1
+      // ptr now points to C
+      ptr ++;
+      string col = "";
+      while(ptr < s.size()){
+        col += s[ptr++];
+      }
+      res = parseC(1, col) + parseR(1, row);
+    }
+  }
+  return res;
+}
+
+void solve(string s) {
+  string res = parse(s);
+
+  cout << res << endl;
 }
 
 int main()
 {
-
-	#ifndef ONLINE_JUDGE
-	freopen("input.txt","r",stdin);
-	//freopen("output.txt","w",stdout);
-	#endif
-
-	int tc;
-	tc = read(int);
-
-	while(tc--){
-		write(tc);
-	}
-	return 0;
+  int tc;
+  SCD(tc);
+  
+	while (tc --> 0){
+    string s;
+    cin >> s;
+    solve(s);
+  }
+  
+	
 }
 /********  Main() Ends Here *************/
