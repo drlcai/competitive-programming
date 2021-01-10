d96c8e14042144787ebe52f1134e86c3c7219fbc
Leran Cai <chuckcai@yahoo.com>
2021-01-10 21:43:30 +0000

    Update 1467A, B, C

--- CPP-Templates/quickstart.cpp
+++ CPP-Templates/quickstart.cpp
@@ -63,6 +63,7 @@ typedef long int int32;
 typedef unsigned long int uint32;
 typedef long long int int64;
 typedef unsigned long long int uint64;
+typedef vector<int64> VI64;
 
 #define watch(x) cout << (#x) << " is " << (x) << endl;
 
